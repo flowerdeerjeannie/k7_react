850a7b7199b4dc87c0a6c0227ef9c0e19d60d8ca
flowerdeerjeannie <flowerdeer121212@gmail.com>
2024-05-23 09:27:32 +0900

    일기예보

--- src/13/Frcst.js
+++ src/13/Frcst.js
@@ -1,22 +1,23 @@
 import ButtonC from "../UI/ButtonC";
 import TailInput from "./TailInput";
 import TailSelect from "../11/TailSelect";
-
 import getxy from "./getxy.json";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 
 export default function Frcst() {
   const navigate = useNavigate();
-  const [ops, setOps] = useState([]);
+  const [ops, setOps] = useState([]); //tm에 의해 정해진 한 지역의 경도,위도 등의 정보배열-ops
   const [x, setX] = useState();
   const [y, setY] = useState();
   const [area, setArea] = useState();
 
-  const inRef = useRef();
-  const selRef = useRef();
+  const inRef = useRef(); //사용자가 선택하는 날짜 값
+  const selRef = useRef(); //사용자가 선택하는 지역 값
 
-  const handleArea = () => {
+  const handleArea = () => { //사용자가 선택할때마다 달라지는값임 그래서 handleArea 안에 let tm 해줘야함
+    //그래야 selRef=사용자선택값 에 따라서 tm이 다르게 설정될수있게 하기 위해 .. 
+    //selRef가 뭔지에 따라서 tm도 달라지고 tm에 따라서 X.Y값도 달라지므로
     let tm = getxy.filter(item => item["1단계"] === selRef.current.value)
     tm = tm[0];
     console.log(tm)
@@ -24,7 +25,7 @@ export default function Frcst() {
     setY(tm["격자 Y"]);
     setArea(selRef.current.value);
   }
-
+//   handleUrl('단기') -에 의해 여기서의 gubun은 '단기' 또는 '초단기'라는 값을 가지며, 각각 단기 예보와 초단기 예보를 구분하기 위해서! 사용함
   const handleUrl = (gubun) => {
     if (!x || !y || !inRef.current.value) {
       alert('날짜와 지역을 선택하세요');
@@ -58,6 +59,7 @@ export default function Frcst() {
           <TailSelect id="sel"
                       ops={ops}
                       selRef={selRef}
+                      // selRef-사용자가 선택할수있는 지역값들 
                       initText="--- 지역선택 ---"
                       handleChange={handleArea} />
           <ButtonC caption="초단기예보"
@@ -66,6 +68,7 @@ export default function Frcst() {
           <ButtonC caption="단기예보"
                     bcolor="sky"
                     handleClick={() => handleUrl('단기')} />
+                    {/* 버튼이 클릭될 때 '단기'라는 문자열을 전달하면서 함수를 실행함*/}
         </div>
       </div>
     </div>
--- src/13/FrcstList.js
+++ src/13/FrcstList.js
@@ -8,16 +8,23 @@ import { TbHttpOptions } from "react-icons/tb";
 export default function FrcstList() {
   // navigate(`/Frcstlt?gubun=${gubun}&x=${x}&y=${y}&dt=${inRef.current.value.replaceAll('-','')}&area=${area}`)
   
+  // URL에서 쿼리 파라미터를 읽기 위해 useSearchParams 훅 사용
   const [sParams] = useSearchParams();
+
+
+  // URL에서 각 파라미터 값을 가져옴. sParams.get('key')를 사용하여 각 파라미터 값을 가져오는거임
+  //URL이 http://example.com/?gubun=단기&x=60&y=127&dt=20240523&area=서울일 때, 
+  //sParams.get('gubun')은 '단기' 값을 반환합니다.
+  //useEffect에서 사용하여 동적으로 제어하기 위하여 sParams에서 가져오는것
   const gubun = sParams.get('gubun');
   const x = sParams.get('x');
   const y = sParams.get('y');
   const dt = sParams.get('dt');
   const area = sParams.get('area');
-
+  
+  // 상태 변수와 레퍼런스를 선언
   const [tdata, setTdata] = useState();
   const [ops, setOps] = useState([]);
-
   const selRef = useRef();
   
   const handleSelect = () => {
@@ -37,7 +44,8 @@ export default function FrcstList() {
                                                     : item["예보구분"] === '초단기예보')
                     .map(item => item["항목명"]);
     setOps(tm);
-                                                  
+     
+    // 'gubun' 파라미터에 따라 필터링된 항목명을 설정
     let url ;
     if (gubun==='초단기') {
       url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?` ;
